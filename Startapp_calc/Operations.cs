e269a8b61d746b8bd35f4845d9d58eccf5514ff6
abreham <aynuyeabresh@gmail.com>
2022-02-17 16:22:52 -0300

    Add project files.

---
+++ Startapp_calc/Operations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Calculation
+{
+    public static double Result;
+    public static double DoOperation(double num1, double num2, int opt)
+    {
+        switch (opt)
+        {
+            case (1):
+                Result =num1 + num2;
+                break;
+            case (2):
+                Result = num1 - num2;
+                Console.Write($"the Substraction result is {num1 - num2}");
+                break;
+            case (3):
+                Result = num1 * num2;
+                Console.Write($"the Multiplicationresult is {num1 * num2}");
+                break;
+            case (4):
+                if (num2 != 0)
+                    {
+                    Result = num1 / num2;
+                }
+                else
+                {
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine("please Enter a non-zero divisor: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        Result = num1 / num2;
+                    }
+
+                }
+                break;
+
+            default:
+
+                break;
+        }
+        return Result;
+        
+    }
+    
+
+}
---
+++ Startapp_calc/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace MyNamespace
+{
+    class calculation
+    {
+        public static double Result;
+        public static void Main(string[] args)
+        {
+            double num1, num2, opt;
+
+            Console.Write("\n\n");
+            Console.Write("A menu driven program for a simple calculator:\n");
+            Console.Write("------------------------------------------------");
+            Console.Write("\n\n");
+            while (true)
+            {
+                Console.Write("enter the first number:");
+                num1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter the second number:");
+                num2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("---------------------OPERATIONS---------------------------");
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("\t\t\t=>enter 1 for Addition");
+                Console.WriteLine("\t\t\t=>enter 2 for substraction");
+                Console.WriteLine("\t\t\t=>enter 3 for multiplication");
+                Console.WriteLine("\t\t\t=>enter 4 for Division");
+                Console.WriteLine("\t\t\t=>enter 0 for Quit");
+                Console.WriteLine("------------------------------------------------");
+                opt = Convert.ToInt32(Console.ReadLine());
+                if (opt == 0)
+                {
+                    Environment.Exit(0);
+                }
+                Result = Calculation.DoOperation(num1, num2, Convert.ToInt32(opt));
+
+                Console.WriteLine($"the Result of your calculation is {Result}");
+
+            }
+
+
+        }
+
+    }
+
+}
